aa47accade6c532c5ebb6fba65b3a44b6092b23f
Jon Rafkind <jon@rafkind.com>
2025-06-07 20:46:14 -0700

    read pattern data

--- mod/loader.go
+++ mod/loader.go
@@ -4,15 +4,29 @@ import (
     "io"
     "bytes"
     "log"
+    "fmt"
 )
 
 type ModFile struct {
     Channels int
     Name string
+    Patterns []Pattern
+}
+
+type PatternData struct {
+    SampleNumber byte // 0-31
+    PeriodFrequency uint16 // 0-65535
+    EffectNumber byte // 0-15
+    EffectParameter byte // 0-255
+}
+
+type Pattern struct {
+    // array of channels, each channel has 64 entries
+    Data [][]PatternData
 }
 
 // little endian 16-bit word
-func readUint16(reader io.ReadSeeker) (uint16, error) {
+func readUint16(reader io.Reader) (uint16, error) {
     var buf [2]byte
     _, err := io.ReadFull(reader, buf[:])
     if err != nil {
@@ -21,7 +35,7 @@ func readUint16(reader io.ReadSeeker) (uint16, error) {
     return (uint16(buf[0]) | (uint16(buf[1]) << 8)), nil
 }
 
-func readByte(reader io.ReadSeeker) (byte, error) {
+func readByte(reader io.Reader) (byte, error) {
     var buf [1]byte
     _, err := io.ReadFull(reader, buf[:])
     if err != nil {
@@ -55,6 +69,8 @@ func Load(reader io.ReadSeeker) (*ModFile, error) {
     } else if bytes.Equal(kind, []byte{'8', 'C', 'H', 'N'}) {
         channels = 8
         log.Printf("Detected 8 channel mod")
+    } else {
+        return nil, fmt.Errorf("Not a mod file")
     }
 
     _, err = reader.Seek(0, io.SeekStart)
@@ -107,8 +123,80 @@ func Load(reader io.ReadSeeker) (*ModFile, error) {
         log.Printf("Sample %v: Name='%s', Length=%d, FineTune=%d, Volume=%d, LoopStart=%d, LoopLength=%d", i, string(sampleName), sampleLength, fineTune, volume, loopStart, loopLength)
     }
 
+    orderCount, err := readByte(reader)
+    if err != nil {
+        return nil, err
+    }
+
+    _, err = readByte(reader)
+    if err != nil {
+        return nil, err
+    }
+
+    var orders [][]byte
+
+    patternMax := 0
+    log.Printf("Reading %v orders", orders)
+    for i := range orderCount {
+        orderBytes := make([]byte, 128)
+        _, err := io.ReadFull(reader, orderBytes)
+        if err != nil {
+            return nil, fmt.Errorf("Could not read order %v: %v", i, err)
+        }
+
+        for value := range orderBytes {
+            patternMax = max(patternMax, int(orderBytes[value]))
+        }
+
+        orders = append(orders, orderBytes)
+    }
+
+    log.Printf("Pattern max: %v", patternMax)
+
+    // read patterns
+    // a pattern consists of channels * 64 entries, where each channel has 64 entries
+    // an entry is a sample to play, combined with an effect and pitch
+    patternBytes := make([]byte, 4)
+    var patterns []Pattern
+    for i := range patternMax {
+        log.Printf("Reading pattern %d", i)
+
+        var patternData [][]PatternData
+        for range channels {
+
+            var data []PatternData
+            for range 64 {
+                _, err = io.ReadFull(reader, patternBytes)
+                if err != nil {
+                    return nil, fmt.Errorf("Could not read pattern data: %v", err)
+                }
+
+                sampleNumber := (patternBytes[0] & 0xf) + (patternBytes[2] >> 4)
+                periodFrequency := (uint(patternBytes[0] & 0xf) << 8) + uint(patternBytes[1])
+                effectNumber := patternBytes[2] & 0xf
+                effectParameter := patternBytes[3]
+
+                log.Printf("Pattern data: Sample=%d, PeriodFrequency=%d, EffectNumber=%d, EffectParameter=%d", sampleNumber, periodFrequency, effectNumber, effectParameter)
+
+                data = append(data, PatternData{
+                    SampleNumber: sampleNumber,
+                    PeriodFrequency: uint16(periodFrequency),
+                    EffectNumber: effectNumber,
+                    EffectParameter: effectParameter,
+                })
+            }
+
+            patternData = append(patternData, data)
+        }
+
+        patterns = append(patterns, Pattern{
+            Data: patternData,
+        })
+    }
+
     return &ModFile{
         Channels: channels,
+        Patterns: patterns,
         Name: string(name),
     }, nil
 }
